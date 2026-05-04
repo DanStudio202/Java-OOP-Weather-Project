013dc422b3349a6de680e28d034e778b6e5d38f1
Dan Saieh <danstudiolead@gmail.com>
2026-05-04 15:11:11 -0500

    Update Authenticate.java

--- src/Authenticate.java
+++ src/Authenticate.java
@@ -1,18 +1,32 @@
+import java.util.ArrayList;
 
 public class Authenticate {
-    
+
+    // simulated database -Dan
+    private ArrayList<String> userList;
+    private ArrayList<String> passList;
 
     public Authenticate(){
-
+        userList = new ArrayList<>();
+        passList = new ArrayList<>();
     }
 
-    public boolean checkLogin(){
+    // handles the actual verification -Dan
+    public boolean checkLogin(String u, String p){
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).equals(u) && passList.get(i).equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        boolean valid = false;
-        return valid;
+    public void registerUser(String u, String p) {
+        userList.add(u);
+        passList.add(p);
     }
 
     public void displayError(){
-
+        System.out.println("Invalid login credentials.");
     }
 }
