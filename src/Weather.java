7821b771f10f69bdc9f1a9af25c8d9329829d94b
Dan Saieh <danstudiolead@gmail.com>
2026-05-04 20:49:39 -0500

    Add JavaDoc comments to Weather class
    Added JavaDoc comments to the Weather class and its methods.

--- src/Weather.java
+++ src/Weather.java
@@ -1,4 +1,7 @@
-
+/**
+ * This represents our weather status for very specific location.
+ * Stores temperature, humidity, wind speed, and general conditions.
+ */
 public class Weather {
 
     private String city;
@@ -8,6 +11,14 @@ public class Weather {
     private double windSpeed; //added from API
     //private Clothes clothes;
 
+    /**
+     * Constructs a Weather data object.
+     * * @param city      The name of the city.
+     * @param temp      The temperature in Fahrenheit.
+     * @param humidity  The humidity percentage.
+     * @param condition A brief description of weather conditions (e.g., "clear sky").
+     * @param windSpeed The wind speed in mph.
+     */
     public Weather(String city, double temp, double humidity, String condition, double windSpeed){
         this.city = city;
         this.temp = temp;
@@ -17,10 +28,18 @@ public class Weather {
         //this.clothes = clothes;
     }
 
+    /**
+     * Gets the current temperature.
+     * * @return The temperature as a double.
+     */
     public double getTemp() {
        return temp; 
     }
-
+    /**
+     * Generates a string report of the weather data.
+     * * @return A formatted string containing all weather attributes.
+     */
+    
     //Print weather objects data - Jason S
     //Split into different gettersfor clearner report - Jason S
     public String getReport() {
